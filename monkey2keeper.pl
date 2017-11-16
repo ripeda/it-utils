8aaea13473474adca6740d7e6e140231b1b448fd
Tami Miguens <tami@ripeda.com>
2017-11-16 11:52:05 -0700

    INITIAL REVISION
    Perl script converts MonkeyBox CSV file to Keeper-Friendly CSV file for import without manual adjustment of column headers.
    All columns in MonkeyBox are retained as custom fields.
    SharedFolder is not used.

---
+++ monkey2keeper.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/env perl
+use Text::CSV;
+
+@ARGV == 2 or die "error: <monkeybox_csv> <client_name>\n\n";
+
+my $folder_root = $ARGV[1];
+$folder_root =~ s/\s+/-/g;
+
+open(CSV, "<$ARGV[0]") or die "error: unable to read $ARGV[0]\n\n";
+
+my $csv = Text::CSV->new ({ binary => 1 });
+
+my $row = $csv->getline(CSV);
+my @th = @$row;
+
+while($row = $csv->getline(CSV)) {
+
+  my @td = @$row;
+
+  $folder = $folder_root;
+  my @search = grep { /^top secret$/ } @td;
+  @search && do { $folder .= "__TOPSECRET"; };
+
+  my %hash = ();
+  my @fcols = ($folder,'','','','','','');
+  for(my $i=0; $i < @th; $i++) {
+    if($th[$i] =~ /^title$/i)                { $fcols[1] = "\"$td[$i]\""; }
+    elsif($th[$i] =~ /^username_or_email$/i) { $fcols[2] = "\"$td[$i]\""; }
+    elsif($th[$i] =~ /^password$/i)          { $fcols[3] = "\"$td[$i]\""; }
+    elsif($th[$i] =~ /^url$/i)               { $fcols[4] = "\"$td[$i]\""; }
+    elsif($th[$i] =~ /^notes$/i)             { $fcols[5] = "\"$td[$i]\""; }
+    else {
+        if($td[$i] !~ /^\".*\"$/) { $td[$i] = "\"$td[$i]\""; }
+        $hash{$th[$i]} = $td[$i];
+    }
+  }
+
+  my @ccols = ();
+  foreach my $key (sort {$a cmp $b} keys %hash) {
+      push @ccols, $key, $hash{$key};
+  }
+
+  printf("%s\n", join ",", @fcols, @ccols);
+
+}
+
+close(CSV)
